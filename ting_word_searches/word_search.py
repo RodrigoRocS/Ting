2fff51e24faec3294bf8cc8a206ed9027a306b09
root <root@DESKTOP-DFB6OFB>
2023-09-28 18:52:52 -0300

    feat: task 7 and 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,46 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for i in range(len(instance)):
+        file_info = instance.search(i)
+        file_name = file_info["nome_do_arquivo"]
+        lines = file_info["linhas_do_arquivo"]
+        occurrences = []
+
+        for line_number, line in enumerate(lines, start=1):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": line_number})
+
+        if occurrences:
+            results.append({
+                "palavra": word,
+                "arquivo": file_name,
+                "ocorrencias": occurrences
+            })
+
+    return results
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for i in range(len(instance)):
+        file_info = instance.search(i)
+        lines = file_info["linhas_do_arquivo"]
+        occurrences = []
+
+        for line_number, line_content in enumerate(lines, start=1):
+            if word.lower() in line_content.lower():
+                occurrences.append({
+                    "linha": line_number, "conteudo": line_content
+                    })
+
+        if occurrences:
+            result = {
+                "palavra": word,
+                "arquivo": file_info["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            }
+            results.append(result)
+
+    return results
